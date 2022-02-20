99310d3fe8fd36561b9cf2ce85d2322dcb5cca8d
k1tt3nz <k1tt3nzpink@gmail.com>
2022-02-20 13:34:01 +0300

    exercise1

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include "matrix.h"
 
+void exercise1() {
+    matrix m = getMemoryMatrix(3, 3);
+    inputMatrix(m);
+
+    position posMin = getMinValuePos(m);
+    position posMax = getMaxValuePos(m);
+    swapRows(m, posMin.rowIndex, posMax.rowIndex);
+}
+
 int main() {
+    exercise1();
 
     return 0;
 }
