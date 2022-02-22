08510e95be6f186b93f7554687058594d6547409
k1tt3nz <k1tt3nzpink@gmail.com>
2022-02-22 16:29:22 +0300

    exercise2

--- main.c
+++ main.c
@@ -32,6 +32,32 @@ void test_exercise1() {
     freeMemoryMatrix(m2);
 }
 
+void test_exercise2() {
+    matrix m1 = createMatrixFromArray(
+            (int[]) {
+                    7, 1, 2,
+                    1, 8, 1,
+                    3, 2, 3
+            },
+            3, 3
+    );
+
+    sortRowsByMaxElement(m1);
+
+    matrix m2 = createMatrixFromArray(
+            (int[]) {
+                    3, 2, 3,
+                    7, 1, 2,
+                    1, 8, 1
+            },
+            3, 3
+    );
+
+    assert(twoMatricesEqual(m1, m2));
+    freeMemoryMatrix(m1);
+    freeMemoryMatrix(m2);
+}
+
 void test_exercise4_one() {
     matrix m1 = createMatrixFromArray(
             (int[]) {
@@ -76,11 +102,13 @@ void test_exercise4_NoSymmetric() {
     freeMemoryMatrix(m1);
 }
 
-void test(){
+void test() {
     test_exercise1();
+    test_exercise2();
     test_exercise4_one();
-    test_exercise4_NoSymmetric()
+    test_exercise4_NoSymmetric();
 }
+
 int main() {
     test();
     return 0;
